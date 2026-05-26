f6f5138c74590823077bacef50564cdc047a51a9
Marcos Ferrari <marcosferrari0801@gmail.com>
2026-05-26 15:44:18 -0300

    feat: add task description and input validations

--- TaskManager-API/Controllers/TaskController.cs
+++ TaskManager-API/Controllers/TaskController.cs
@@ -13,28 +13,40 @@ public class TaskController : ControllerBase
     [HttpGet] // acesso via GET para obter a lista de tarefas
     public IActionResult GetAction()
     {
-            return Ok(tasks); // retorna o status 200 OK e a lista de tarefas no corpo da resposta
+        return Ok(tasks); // retorna o status 200 OK e a lista de tarefas no corpo da resposta
     }
 
     [HttpPost] // acesso via POST para criar uma nova tarefa
     public IActionResult CreateTask(TaskItem task)
     {
-            tasks.Add(task);
-            return Created("", task); // retorna o status 201 Created e a tarefa criada
+        if (task == null)
+        {
+            return BadRequest("A tarefa não pode ser nula!");
+        }
+        if (string.IsNullOrWhiteSpace(task.Title))
+        {
+            return BadRequest("É necessário informar o título da tarefa!");
+        }
+        if (task.Description?.Length > 600) // ?. só acessa Length se Description não for nulo
+        {
+            return BadRequest("A descrição da tarefa deve ter no máximo 600 caracteres!");
+        }
+        tasks.Add(task);
+        return Created("", task); // retorna o status 201 Created e a tarefa criada
     }
 
     [HttpGet("{id}")]  // acesso via GET para obter uma tarefa específica pelo ID, ex: /tasks/{id}
-    public IActionResult GetTaskById(int id) 
+    public IActionResult GetTaskById(int id)
     {
         // firstOrDefault retorna a primeira tarefa que corresponde ao ID ou null se não encontrar
         // t é a variável de iteração que representa cada tarefa na lista tasks
-        var task = tasks.FirstOrDefault(t => t.Id == id); 
+        var task = tasks.FirstOrDefault(t => t.Id == id);
 
         if (task == null) // verificação
-            {
-                return NotFound();
-            }
-            return Ok(task);
+        {
+            return NotFound();
+        }
+        return Ok(task);
     }
 
     [HttpDelete("{id}")]
@@ -42,13 +54,37 @@ public class TaskController : ControllerBase
     {
         var task = tasks.FirstOrDefault(t => t.Id == id);
 
-            if(task == null)
-            {
-                return NotFound();
-            }
-            tasks.Remove(task);
-            return NoContent(); // retorna o status 204 No Content para indicar que a tarefa foi deletada com sucesso, sem retornar nenhum conteúdo na resposta
+        if (task == null)
+        {
+            return NotFound();
+        }
+        tasks.Remove(task);
+        return NoContent(); // retorna o status 204 No Content para indicar que a tarefa foi deletada com sucesso, sem retornar nenhum conteúdo na resposta
 
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateTaskById(int id, TaskItem updatedTask) // logica parecida com a de GetTaskById, mas para atualizar a tarefa
+    {
+        var task = tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task == null)
+        {
+            return NotFound();
+        }
+        if (String.IsNullOrWhiteSpace(updatedTask.Title))
+        {
+            return BadRequest("É necessário informar o título da tarefa!");
+        }
+        if (updatedTask.Description?.Length > 600)
+        {
+            return BadRequest("A descrição da tarefa deve ter no máximo 600 caracteres!");
+        }
+        task.Title = updatedTask.Title;
+        task.Completed = updatedTask.Completed;
+        task.Description = updatedTask.Description;
+        // o id normalmente não se altera por ser unico
+        return NoContent();
     }
 }
 
--- TaskManager-API/Models/TaskItem.cs
+++ TaskManager-API/Models/TaskItem.cs
@@ -6,5 +6,7 @@ public class TaskItem
 
     public string Title { get; set; }
 
+    public string? Description { get; set; }
+
     public bool Completed { get; set; }
 }
